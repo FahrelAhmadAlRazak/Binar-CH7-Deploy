539208816d6eb43afba78a2c523368b6443ba5ad
FahrelAhmadAlRazak <114165391+FahrelAhmadAlRazak@users.noreply.github.com>
2023-11-19 19:18:29 +0700

    fix : env

--- config/database.js
+++ config/database.js
@@ -1,19 +1,18 @@
+require('dotenv').config();
 
 const {
   DB_USERNAME = "postgres",
   DB_PASSWORD = "1Eb2adB6Dbd*Gf55Dd*CAa3cCCg*3561",
   DB_HOST = "viaduct.proxy.rlwy.net",
-  DB_NAME ="railway"
+  DB_NAME = "railway"
 } = process.env;
 
 module.exports = {
-
-
   "development": {
     "username": DB_USERNAME,
-    "password":  DB_PASSWORD,
+    "password": DB_PASSWORD,
     "database": DB_NAME,
-    "host":  DB_HOST,
+    "host": DB_HOST,
     "port": 33737,
     "dialect": "postgres"
   },
@@ -32,4 +31,4 @@ module.exports = {
     "port": 33737,
     "dialect": "postgres"
   }
-}
+};
